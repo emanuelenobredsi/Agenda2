6a09f1571d198d155de0d165f10ca699db2c205b
Emanuele Nobre <emanuelenobreoliveira@gmail.com>
2026-08-16 19:58:03 -0300

    Formulario com php

---
+++ cadastro.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<title>Formulario</title>
+</head>
+<body>
+<form id="form1" name="form1" method="post" action="processaCadastro.php">
+<br>
+ Nome Completo<input name="nomecompl" type="text" id="nomecompl" placeholder = "Digitar
+Nome"><br>
+Idade<input name="idade" type="text" id="idade" placeholder = "Ex.: 18"><br>
+Profissão<input name="prof" type="text" id="prof" placeholder = "Ex:
+Professor"><br>
+Salário Pretendido<input name="salpretd" type="text"id="salpretd" placeholder = "Ex: 1000"><br>
+Experiência anterior<input name="exp" type="text"id="exp" placeholder = "Digite Aqui"><br>
+<input name="Enviar" type="submit" id="enviar" value="enviar">
+</form>
+</body>
+</html>
---
+++ processaCadastro.php
@@ -0,0 +1,7 @@
+<?php
+echo "Nome Completo: ".$_POST['nomecompl']."<br>";
+echo "Idade: ".$_POST['idade']."<br>";
+echo "Profissão: ".$_POST['prof']."<br>";
+echo "Salário Pretendido: R$ ".$_POST['salpretd']."<br>";
+echo "Experiência anterior: ".$_POST['exp']."<br>";
+?>
